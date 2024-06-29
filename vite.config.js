eacad74814ce620cdba28792c05e773d7702d20e
wasiliy <wasiliy.dev@gmail.com>
2024-06-29 10:57:23 +0300

    Added eslint, prettier, stylelint

---
+++ vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  root: "./src",
+  server: {
+    port: 3000,
+    open: true,
+  },
+  preview: {
+    port: 8080,
+    open: true,
+  },
+  build: {
+    outDir: "../dist",
+  },
+});
