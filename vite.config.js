d3bde5e2a1c1a97afe1032a92f09080e820614fc
wasiliy <wasiliy.dev@gmail.com>
2024-06-29 12:09:26 +0300

    Added sass

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: "./src",
-  server: {
-    port: 3000,
-    open: true,
-  },
-  preview: {
-    port: 8080,
-    open: true,
-  },
-  build: {
-    outDir: "../dist",
-  },
+    root: './src',
+    server: {
+        port: 3000,
+        open: true,
+    },
+    preview: {
+        port: 8080,
+        open: true,
+    },
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+    },
 });
