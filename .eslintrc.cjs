6f4cd496f3154cba245c287a39dd1d7c665bae16
wasiliy <wasiliy.dev@gmail.com>
2024-08-07 15:57:57 +0300

    Added format script

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,25 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-  overrides: [
-    {
-      env: {
-        node: true,
-      },
-      files: ['.eslintrc.{js,cjs}'],
-      parserOptions: {
-        sourceType: 'script',
-      },
+    env: {
+        browser: true,
+        es2021: true,
     },
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-    'prettier/prettier': 'error',
-  },
+    extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: ['.eslintrc.{js,cjs}'],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    rules: {
+        'prettier/prettier': 'error',
+    },
 };
