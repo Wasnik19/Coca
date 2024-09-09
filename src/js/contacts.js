32eb880cc54ccb41929569ab1e329a73d9e9bea0
wasiliy <wasiliy.dev@gmail.com>
2024-09-09 14:13:48 +0300

    Creted section Map and section Partners

---
+++ src/js/contacts.js
@@ -0,0 +1,10 @@
+import '../assets/scss/contacts.scss';
+
+// components
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+import { usePartnersMobileSlider } from './components/home/slider.js';
+
+useTheme();
+useBurger();
+usePartnersMobileSlider();
--- vite.config.js
+++ vite.config.js
@@ -20,6 +20,7 @@ export default defineConfig({
             input: {
                 main: './src/index.html',
                 pricing: './src/pricing.html',
+                contats: './src/contacts.html',
             },
         },
     },
