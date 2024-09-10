1fb2a317027c145aaff27d256f22ee6bffe3dc5a
wasiliy <wasiliy.dev@gmail.com>
2024-09-10 18:14:59 +0300

    Crated get started section

--- src/js/contacts.js
+++ src/js/contacts.js
@@ -4,7 +4,9 @@ import '../assets/scss/contacts.scss';
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
 import { usePartnersMobileSlider } from './components/home/slider.js';
+import { usePhone } from './components/contacts/phone.js';
 
 useTheme();
 useBurger();
 usePartnersMobileSlider();
+usePhone();
